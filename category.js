a0bf068b43e6ce53d748e12e6b92b72df279b9cf
Lee <S1630B@corp1.ad1.seb.net>
2018-06-13 16:57:12 +0200

    add go

--- category.js
+++ category.js
@@ -5,6 +5,23 @@ window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
   description: "SEBShift - it's a shift"
 }];
 
+var category = _.find(window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES,
+                      { id: 'languages' });
+// Add Go as a new subcategory under Languages.
+category.subCategories.splice(2,0,{ // Insert at the third spot.
+  // Required. Must be unique.
+  id: "go",
+  // Required.
+  label: "Go",
+  // Optional. If specified, defines a unique icon for this item.
+  icon: "icon-go-gopher",
+  // Required. Items matching any tag will appear in this subcategory.
+  tags: [
+    "go",
+    "golang"
+  ]
+});
+
 window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
   id: "sebshift",
   label: "SEBShift",
@@ -12,7 +29,7 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
     {
       id: "jenkins",
       label: "jenkins",
-      icon: "icon-wildfly",
+      icon: "fa fa-refresh",
       tags: [
         "sebshift",
         "jenkins",
