b7fdb19be90392dad192513f1c1880808eea17d7
Lee <S1630B@corp1.ad1.seb.net>
2018-06-14 14:26:42 +0200

    minor change icon

--- category.js
+++ category.js
@@ -1,26 +1,3 @@
-window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
-  title: "SEBShift",
-  icon: "fa fa-dashboard",
-  url: "http://sebshift.com",
-  description: "SEBShift - it's a shift"
-}];
-
-var category = _.find(window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES,
-                      { id: 'languages' });
-// Add Go as a new subcategory under Languages.
-category.subCategories.splice(2,0,{ // Insert at the third spot.
-  // Required. Must be unique.
-  id: "go",
-  // Required.
-  label: "Go",
-  // Optional. If specified, defines a unique icon for this item.
-  icon: "icon-go-gopher",
-  // Required. Items matching any tag will appear in this subcategory.
-  tags: [
-    "go",
-    "golang"
-  ]
-});
 
 window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
   id: "sebshift",
@@ -29,21 +6,20 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
     {
       id: "jenkins",
       label: "jenkins",
-      icon: "fa fa-refresh",
+      icon: "icon-openshift",
       tags: [
         "sebshift",
-        "jenkins",
-        "wildfly"
+        "jenkins"
       ]
     },
 	{
       id: "sebshift",
       label: "SEBShift",
-      icon: "icon-go-gopher",
+      icon: "fa fa-fork",
       tags: [
         "sebshift",
 		"go",
-		"golang"
+		"fork"
       ]
     }
   ]
