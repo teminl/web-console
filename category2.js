c44e57cff21dd74de96258ecc764ed776e6319d0
Lee <S1630B@corp1.ad1.seb.net>
2018-06-15 11:37:28 +0200

    johans js

---
+++ category2.js
@@ -0,0 +1,22 @@
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
+  title: "SEBShift",
+  icon: "fa fa-dashboard",
+  url: "http://sebshift.com",
+  description: "SEBShift - it's a shift"
+}];
+
+window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
+  id: "sebshift",
+  label: "SEBShift",
+  subCategories: [
+    {
+      id: "sebshift",
+      label: "SEBShift",
+      icon: "icon-openshift",
+      tags: [
+        "sebshift",
+        "jenkins"
+      ]
+    }
+  ]
+});
