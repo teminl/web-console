e97fd5cd456076e4d26d9fa40f5f6db81da399c9
Lee <S1630B@corp1.ad1.seb.net>
2018-06-18 09:48:23 +0200

    show featured application

--- category2.js
+++ category2.js
@@ -3,6 +3,16 @@ window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
   icon: "fa fa-dashboard",
   url: "http://sebshift.com",
   description: "SEBShift - it's a shift"
+  }, {
+  title: "System Status",
+  icon: "fa fa-heartbeat",
+  url: "http://example.com/status",
+  description: "View system alerts and outages."
+  }, {
+  title: "Confluence",
+  icon: "fa fa-edit",
+  url: "https://confluence-general.sebank.se/display/ses",
+  description: "Documentation etc"
 }];
 
 window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
@@ -16,11 +26,12 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
       tags: [
         "sebshift",
         "jenkins",
-		"MongoDB",
 		"Python",
 		"Node.js"
-		"jhpliberty",
-		"tomcat"
+		"Liberty",
+		"tomcat",
+		"JaaS",
+		"OpenJDK"
       ]
     }
   ]
